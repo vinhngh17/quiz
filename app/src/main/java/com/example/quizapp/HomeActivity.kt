1b5d3f6d64ac0a18b57cdcc117f047db9e4ea211
vinhngh17 <nghvinh12345@gmail.com>
2023-03-31 08:04:48 +0700

    nav graph home

--- app/src/main/java/com/example/quizapp/HomeActivity.kt
+++ app/src/main/java/com/example/quizapp/HomeActivity.kt
@@ -3,35 +3,25 @@ package com.example.quizapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.example.quizapp.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomeBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        replaceFragment(HomeFragment())
 
-        binding.bottomNagivationView.setOnClickListener{
-
-                when(it.id){
-                    R.id.home -> replaceFragment(HomeFragment())
-                    R.id.folder_shared -> replaceFragment(FolderFragment())
-                    R.id.personal -> replaceFragment(PersonalFragment())
-                    R.id.search -> replaceFragment(SearchFragment())
-                }
-
-            }
-        }
-
-
-    private fun replaceFragment(fragment: Fragment) {
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.frame_layout, fragment)
-        fragmentTransaction.commit()
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.homeFragmentContainer) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNagivationView.setupWithNavController(navController)
     }
 }
