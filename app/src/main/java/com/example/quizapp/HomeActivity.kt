f205bb4964ebd82641f70a03064632d98338bc63
vinhngh17 <nghvinh12345@gmail.com>
2023-04-13 23:29:06 +0700

    Home frag commit

--- app/src/main/java/com/example/quizapp/HomeActivity.kt
+++ app/src/main/java/com/example/quizapp/HomeActivity.kt
@@ -2,17 +2,11 @@ package com.example.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.quizapp.databinding.ActivityHomeBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHomeBinding
